2f472811824a27d54fbbd518280802ab6bed7038
Utkarsh Singh <us190190@gmail.com>
2012-12-31 03:19:26 +0530

    Updating files

--- Hangman.java
+++ Hangman.java
@@ -1,11 +1,3 @@
-/**
- * Here is a game "Hangman" coded in JAVA. 
- * The source of words is a file containing 15000 words. 
- * As per the user's decision the associated Hangman image 
- * changes or not. This was coded by me in the last semester 
- * of my B.Tech (Jan 2012).
- */
- 
 package hangman;
 
 import java.applet.Applet;
